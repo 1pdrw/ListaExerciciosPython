3637669947418f3870b1a41ecb455ae7328653fb
Pedro <@pedrohenrx1@gmail.com>
2021-12-09 15:45:53 -0300

    Atividades Python

---
+++ Estrutura Sequencial/03.py
@@ -0,0 +1,4 @@
+'''Faça um Programa que peça dois números e imprima a soma.'''
+n1 = int(input('Insira um númer:'))
+n2 = int(input('Insira outro número:'))
+print('A soma dos números que você escolheu é',n1 + n2)
---
+++ Estrutura Sequencial/04.py
@@ -0,0 +1,7 @@
+'''Faça um Programa que peça as 4 notas bimestrais e mostre a média.'''
+n1 = int(input('Insira sua nota bimestral:'))
+n2 = int(input('Insira outra nota:'))
+n3 = int(input('Insira mais uma nota:'))
+n4 = int(input('Insira suas última nota:'))
+
+print('Sua média semestral é', (n1 + n2 + n3 + n4) / 4)
