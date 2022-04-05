09f1c4db613db7971601a483852c146ba53c5aea
Pedro Henrique <pedrohenrx1@gmail.com>
2022-04-05 18:18:26 -0300

    Atividades Python

--- Estrutura de Repetição/10.py
+++ Estrutura de Repetição/10.py
@@ -1,6 +1,6 @@
 '''Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.'''
 
-
+'''
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um número: '))
 
@@ -9,4 +9,12 @@ for i in range(n1,n2,1):
 
 if n2 < n1:
     print('Segundo número tem que ser maior que o primeiro')
+'''
 
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+
+for i in range(n1 + 1, n2):
+        print(i)
+for i in range(n2 + 1, n1):
+        print(i)
